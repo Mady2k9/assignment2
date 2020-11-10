42a3175e9eb571a9d1af9f2a57f8a28098957c10
madhuresh kumar <mady2k9@gmail.com>
2020-11-10 16:42:36 +0530

    first assignment

---
+++ quat3.py
@@ -0,0 +1,7 @@
+#3. Write a Python program to find the volume of a sphere with diameter 12 cm.
+#Formula: V = 4 / 3 * π * r
+#av=4/3*pie*r**3
+pi=3.141
+r=12/2
+V= 4.0/3.0*pi* r**3
+print('The volume of the sphere is: ',V)
